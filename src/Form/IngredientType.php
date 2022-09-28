ad737ce142a5c844663d1eacb87ea01ad4d4eec7
pierre <pierre.1500@gmail.com>
2022-09-28 14:36:30 +0200

    ✨ Add button for modify or delete ingredients

---
+++ src/Form/IngredientType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'ingrédient',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 50,
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 50,]),
+                    new Assert\NotBlank(),
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix de l\'ingrédient en __',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    new Assert\LessThan(200),
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer mon ingrédient',
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
