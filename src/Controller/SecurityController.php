674cd5fec141a435fb7cbe98490095e83e3666b1
pierre <pierre.1500@gmail.com>
2022-09-29 16:04:06 +0200

    🚧 Setting up register form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/inscription", name="security_registration")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -18,9 +27,42 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/inscription", name="security.register", methods={"GET", "POST"})
+     */
     #[Route('/deconnexion', name: 'security.logout')]
     public function logout()
     {
         // Nothing to do here, Symfony will handle the logout automatically.
+    }
+
+    /**
+     * @Route("/inscription", name="security.register")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/inscription', name: 'security.register', methods: ['GET', 'POST'])]
+    public function register(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = new User();
+        $user -> setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $user = $form->getData();
+
+           $this->addFlash('success', 'Votre compte a bien été créé !');
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('security.login');
+        }
+
+        return $this->render('pages/security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Ingredient;
 use App\Entity\Recipe;
 use App\Repository\IngredientRepository;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
