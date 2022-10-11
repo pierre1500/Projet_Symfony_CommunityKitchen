043923c70ce22f2eb903439e16424bcf0fccb1e2
Pain_bls <pierre.1500@gmail.com>
2022-10-11 16:15:12 +0200

    ✨ Envoi d'email lorsque formulaire de contact soumis

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,17 +5,20 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
     public function index(
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
     ): Response
     {
         $contact = new Contact();
@@ -32,6 +35,18 @@ class ContactController extends AbstractController
             $contact = $form->getData();
             $entityManager->persist($contact);
             $entityManager->flush();
+
+            //send email
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('pierre.1500@gmail.com')
+                ->subject($contact->getSubject())
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'contact' => $contact
+                ]);
+            $mailer->send($email);
+
 
             $this->addFlash('success', 'Votre message a bien été envoyé.');
 
