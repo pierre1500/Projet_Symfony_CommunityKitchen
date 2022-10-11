3f0766c4c47bf998c9818f78a9e52866682515a8
Pain_bls <pierre.1500@gmail.com>
2022-10-11 16:31:24 +0200

    ✨ Ajout reCaptcha v3 sur formulaire de contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -75,7 +77,11 @@ class ContactType extends AbstractType
                 'label' => 'Envoyer mon message',
                 'attr' => [
                     'class' => 'btn btn-primary mt-4',
-                ]
+                ],
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
             ])
         ;
     }
